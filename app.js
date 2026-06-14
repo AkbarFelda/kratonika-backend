03cca589c9cc5b14a1f0566123889af628314cfc
AkbarFelda <akbarfelda65@gmail.com>
2026-06-14 16:00:49 +0700

    chore: tambah konfigurasi vercel.json untuk deployment

--- app.js
+++ app.js
@@ -6,19 +6,17 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Jalankan middleware global
 app.use(cors());
 app.use(express.json());
 
-// Sambungkan rute utama API ke endpoint /api
 app.use('/api', apiRoutes);
 
-// Rute dasar untuk cek status server
 app.get('/', (req, res) => {
     res.json({ message: "Kratonika API sedang berjalan lancar!" });
 });
 
-// Jalankan server
 app.listen(PORT, () => {
     console.log(`Server [kratonika-backend] aktif di port: ${PORT}`);
 });
+
+module.exports = app;
